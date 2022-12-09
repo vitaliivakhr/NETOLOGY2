d0deedb13aeb8434e6f85cbe205fe595775449b2
Vitalii Vakhromeev <vitaliivakhr@yandex.ru>
2022-12-09 23:06:44 +0200

    7.2-2

--- 7.2/terraform/variables.tf
+++ 7.2/terraform/variables.tf
@@ -12,6 +12,6 @@ variable "yandex_folder_id" {
 
 # Заменить на ID своего образа
 # ID можно узнать с помощью команды yc compute image list
-variable "centos-7-base" {
-  default = "fd84qlekonjn6kou3df0"
-}
+#variable "centos-7-base" {
+ # default = "fd84qlekonjn6kou3df0"
+#}
